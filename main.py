36f85e3e6873a82af568662a45a39e3f76fcd082
Mike Clubb <mclubb@charter.net>
2017-10-11 12:08:47 -0500

    Bonus Work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy 
 from pprint import pprint
+from datetime import datetime
 # Note: the connection string after :// contains the following info:
 # user:password@server:portNumber/databaseName
 app = Flask(__name__)
@@ -15,6 +16,7 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text())
+    entryDate = db.Column(db.DateTime())
 
     def __init__(self, name):
         self.name = name
@@ -26,9 +28,12 @@ def index():
     if 'id' in request.args: 
         id = request.args['id']
         entry = Blog.query.filter_by(id=id).first()
+        entry.body = "<br>".join(entry.body.split('\n'))
         return render_template('singleblog.html', title=entry.title, blog=entry)
      
     entries = Blog.query.order_by(Blog.id.desc()).all()
+    for entry in entries:
+        entry.body = "<br>".join(entry.body.split('\n'))
     return render_template('blog.html', title='My Blog', blogs=entries)
 
 
@@ -38,12 +43,13 @@ def newpost():
         entry = Blog('blog')
         entry.title = request.form['title']
         entry.body = request.form['body']
+        entry.entryDate = datetime.now()
 
         if entry.title == '':
             return render_template('newblog.html', title="New Blog Post", form=request.form, errorMessage="Title is required")
 
         if entry.body == '':
-            return render_template('newblog.html', title="New Blog Post", form=request.form, errorMessage="Body is requried")
+            return render_template('newblog.html', title="New Blog Post", form=request.form, errorMessage="Body is required")
 
         db.session.add(entry)
         db.session.commit()
